3a28707e85ef2961fc924692140c862dd3942603
Nicolas Garneau <ngarneau@ngarneau.com>
2017-12-07 13:31:10 -0500

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Pitoune',
+    version='0.0.1',
+    author='GRAAL',
+    author_email='info@graal.com',
+    packages=['pitoune'],
+    description='Pytorch related utilities.'
+)
