290e1e9bcd5dd258a1dd68dbb8825d160eda3135
Frédérik Paradis <fredy_14@live.fr>
2017-12-14 13:48:21 -0500

    Fix bug in tensors_to_variables

--- pitoune/utils.py
+++ pitoune/utils.py
@@ -24,8 +24,8 @@ def tensors_to_variables(v, *args, **kwargs):
     if torch.is_tensor(v):
         return Variable(v, *args, **kwargs)
     if isinstance(v, list) or isinstance(v, tuple):
-        return type(v)(tensors_to_variables(el) for el in v)
+        return type(v)(tensors_to_variables(el, *args, **kwargs) for el in v)
     if isinstance(v, dict):
-        return {k:tensors_to_variables(el) for k,el in v}
+        return {k:tensors_to_variables(el, *args, **kwargs) for k,el in v}
     if not torch.is_tensor(v):
         raise ValueError("The type '%s' is not supported by this function." % type(v).__name__)
