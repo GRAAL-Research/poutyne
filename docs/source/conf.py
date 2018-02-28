2e11122e38fe39fc1c9e616edbcf49a103ff77d5
Frédérik Paradis <fredy_14@live.fr>
2018-02-28 13:26:34 -0500

    Update version number to 0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ author = 'Frédérik Paradis'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
