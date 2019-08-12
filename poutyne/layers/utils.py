ea1ebaa4a4f8e35ba1d865a9abe7b8a6ea6daed0
Frédérik Paradis <fredy_14@live.fr>
2019-08-12 14:25:33 -0400

    Update to PyTorch 1.2

--- poutyne/layers/utils.py
+++ poutyne/layers/utils.py
@@ -3,26 +3,6 @@
 import torch.nn as nn
 
 
-class Flatten(nn.Module):
-    """
-    Flattens the input tensor such that the dimension 0 is the batch. Equivalent to
-    ``x.flatten(1)``.
-    """
-
-    def forward(self, x):
-        return x.flatten(1)
-
-
-class Identity(nn.Module):
-    """
-    Returns the same tensor as the input. This module can be useful when removing a module from a
-    third-party module.
-    """
-
-    def forward(self, x):
-        return x
-
-
 class Lambda(nn.Module):
     """
     Applies a function to the input tensor.
@@ -34,7 +14,7 @@ class Lambda(nn.Module):
 
         .. code-block:: python
 
-            # Alternate version to the ``Flatten`` module.
+            # Alternate version to the ``nn.Flatten`` module.
             my_flatten = Lambda(lambda x: x.flatten(1))
 
     """
