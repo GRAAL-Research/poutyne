2fb08b6c12e913059380f3e25d5e400afbf6e39b
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2019-08-22 15:31:43 -0400

    Add test for batches_per_step > n_batches

--- tests/framework/test_model.py
+++ tests/framework/test_model.py
@@ -317,6 +317,25 @@ class ModelTest(TestCase):
 
             np.testing.assert_almost_equal(returned_params, expected_params, decimal=4)
 
+    def test_fitting_generator_n_batches_per_step_higher_than_num_batches(self):
+        total_batch_size = 6
+
+        x = torch.rand(1, total_batch_size, 1)
+        y = torch.rand(1, total_batch_size, 1)
+
+        initial_params = self.model.get_weight_copies()
+
+        self.model.fit_generator(list(zip(x, y)), None, epochs=1, batches_per_step=1)
+
+        expected_params = list(self.model.get_weight_copies().values())
+
+        self.model.set_weights(initial_params)
+
+        self.model.fit_generator(list(zip(x, y)), None, epochs=1, batches_per_step=2)
+
+        returned_params = list(self.model.get_weight_copies().values())
+
+        np.testing.assert_almost_equal(returned_params, expected_params, decimal=4)
 
     def test_fitting_generator_n_batches_per_step_uneven_batches(self):
         total_batch_size = 6
@@ -341,7 +360,7 @@ class ModelTest(TestCase):
             splitted_x = x.split(chunk_size)
             splitted_y = y.split(chunk_size)
 
-            n_batches_per_step = ceil(total_batch_size/chunk_size)
+            n_batches_per_step = ceil(total_batch_size / chunk_size)
 
             self.model.fit_generator(list(zip(splitted_x, splitted_y)),
                                      None,
