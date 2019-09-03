8905b676772b333076e3eda6ec24b3e4b80cd483
davebulaval <david.beauchemin.5@ulaval.ca>
2019-09-03 11:25:27 -0400

    forgot to add doc and loss code for label f1 score tracking

--- poutyne/framework/metrics/epoch_metrics.py
+++ poutyne/framework/metrics/epoch_metrics.py
@@ -39,16 +39,33 @@ class EpochMetric(ABC):
 class F1(EpochMetric):
     """
     Wrapper around the Allen NLP FBetaMeasure class.
-    average can be 'micro, macro or none'
+
+    Args:
+        beta : (float), optional (default = 1.0) The strength of recall versus precision in the F-score.
+
+        average : (str) or (int), ['micro' (default), 'macro', label_number]
+        If the arguments is of type integer, The score for this class (the label number) is calculated. Otherwise, this
+        determines the type of averaging performed on all the data:
+        ``'micro'``:
+            Calculate metrics globally by counting the total true positives,
+            false negatives and false positives.
+        ``'macro'``:
+            Calculate metrics for each label, and find their unweighted mean.
+            This does not take label imbalance into account.
 
     """
 
     def __init__(self, beta=1.0, average='micro'):
         super().__init__()
-        self.average = average  # to verify if we can extract a F1 score for a specific label with get_metric method.
         if FBetaMeasure is None:
             raise ImportError("allen nlp need to be installed to use this class.")
-        self.running_measure = FBetaMeasure(beta=beta, average=average)
+
+        self.label = None
+        if isinstance(average, int):
+            self.label = average
+            self.running_measure = FBetaMeasure(beta=beta)
+        else:
+            self.running_measure = FBetaMeasure(beta=beta, average=average)
 
     def __call__(self, y_prediction, y_true):
         mask = None
@@ -57,6 +74,8 @@ class F1(EpochMetric):
         self.running_measure(y_prediction, y_true, mask=mask)
 
     def get_metric(self, reset=True):
+        if self.label is not None:
+            return self.running_measure.get_metric(reset=reset)['fscore'][self.label]
         return self.running_measure.get_metric(reset=reset)['fscore']
 
 
