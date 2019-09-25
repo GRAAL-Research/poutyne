a7a52ccab1c9c2559d39eda95c5ec74cddc08c57
Mathieu Godbout <39306007+TortillasAlfred@users.noreply.github.com>
2019-09-25 14:14:53 -0400

    Update monitor_metric format doc

--- poutyne/framework/experiment.py
+++ poutyne/framework/experiment.py
@@ -63,7 +63,7 @@ class Experiment:
             :class:`~poutyne.framework.metrics.epoch_metrics.EpochMetric`
             (Default value = None)
         monitor_metric (str): Which metric to consider for best model performance calculation. Should be in
-            the format '{val, train}_{metric_name}' (i.e. 'val_loss'). If None, will follow the value suggested
+            the format '{metric_name}' or 'val_{metric_name}' (i.e. 'val_loss'). If None, will follow the value suggested
             by ``task`` or default to 'val_loss'.
             (Default value = None)
         monitor_mode (str): Which mode, either 'min' or 'max', should be used when considering the ``monitor_metric``
