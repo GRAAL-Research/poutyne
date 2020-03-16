931a2759e94a234a49d07215811b74d20f4871b4
Frédérik Paradis <fredy_14@live.fr>
2020-03-16 14:39:00 -0400

    Add example

--- poutyne/framework/model.py
+++ poutyne/framework/model.py
@@ -46,17 +46,29 @@ class Model:
             (Default value = None)
 
     Note:
-        The name of each batch and epoch metric can be change by passing a tuple `(name, metric)` instead
-        of simply the metric function or object, where `name` is the alternative name of the metric.
+        The name of each batch and epoch metric can be change by passing a tuple ``(name, metric)`` instead
+        of simply the metric function or object, where ``name`` is the alternative name of the metric.
 
         Batch and epoch metrics can return multiple metrics (e.g. an epoch metric could return an F1-score
         with the associated precision and recall). The metrics can returned via an iterable (tuple, list,
         Numpy arrays, tensors, etc.) or via a mapping (e.g. a dict). However, in this case, the names of
         the different metric has to be passed in some way. There are two ways to do so. The easiest one
-        is to pass the metric as a tuple `(names, metric)` where `names` is a tuple containing a name for
-        each metric returned. Another way is to overide the attribute `__name__` of the function or object
+        is to pass the metric as a tuple ``(names, metric)`` where ``names`` is a tuple containing a name for
+        each metric returned. Another way is to overide the attribute ``__name__`` of the function or object
         so that it returns a tuple containing a name for all metrics returned. Note that, when the metric
         returns a mapping, the names of the different metrics must be keys in the mapping.
+
+        Example:
+
+        .. code-block:: python
+
+            # Example with custom batch metrics
+            my_custom_metric = lambda input, target: 42.
+            my_custom_metric2 = lambda input, target: torch.tensor([42., 43.])
+            my_custom_metric3 = lambda input, target: {'a': 42., 'b': 43.}
+            batch_metrics = [('custom_name', my_custom_metric),
+                             (('metric_1', 'metric_2'), my_custom_metric2),
+                             (('a', 'b'), my_custom_metric3)]
 
     Attributes:
         network (torch.nn.Module): The associated PyTorch network.
