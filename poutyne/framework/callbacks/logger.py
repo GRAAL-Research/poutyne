68f97d6a1a08000f6ce7dc7e5e102ba8bb156380
davebulaval <david.beauchemin.5@ulaval.ca>
2020-03-19 17:09:15 -0400

    yapf pylint

--- poutyne/framework/callbacks/logger.py
+++ poutyne/framework/callbacks/logger.py
@@ -144,14 +144,14 @@ class AtomicCSVLogger(Logger):
 
     def _on_train_begin_write(self, logs: Dict):
         if not self.append:
-            atomic_lambda_save(self.filename, self._save_log, (None,), temporary_filename=self.temporary_filename)
+            atomic_lambda_save(self.filename, self._save_log, (None, ), temporary_filename=self.temporary_filename)
 
     def _on_train_batch_end_write(self, batch_number: int, logs: Dict):
-        atomic_lambda_save(self.filename, self._save_log, (logs,), temporary_filename=self.temporary_filename)
+        atomic_lambda_save(self.filename, self._save_log, (logs, ), temporary_filename=self.temporary_filename)
 
     def _on_epoch_end_write(self, epoch_number: int, logs: Dict):
         logs = dict(logs, lr=self._get_current_learning_rates())
-        atomic_lambda_save(self.filename, self._save_log, (logs,), temporary_filename=self.temporary_filename)
+        atomic_lambda_save(self.filename, self._save_log, (logs, ), temporary_filename=self.temporary_filename)
 
 
 class TensorBoardLogger(Logger):
@@ -201,7 +201,7 @@ class TensorBoardLogger(Logger):
         for k, v in grouped_items.items():
             self.writer.add_scalars(k, v, epoch_number)
         lr = self._get_current_learning_rates()
-        if isinstance(lr, (list,)):
+        if isinstance(lr, (list, )):
             self.writer.add_scalars('lr', {str(i): v for i, v in enumerate(lr)}, epoch_number)
         else:
             self.writer.add_scalars('lr', {'lr': lr}, epoch_number)
