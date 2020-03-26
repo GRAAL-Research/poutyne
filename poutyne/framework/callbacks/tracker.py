fa464b975cbc5bfdd0e015097f80a4a90ee60fc5
davebulaval <david.beauchemin.5@ulaval.ca>
2020-03-26 12:24:48 -0400

    better type management for batc_statistic_upgrade

--- poutyne/framework/callbacks/tracker.py
+++ poutyne/framework/callbacks/tracker.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Iterable
 
 import numpy as np
 import torch
@@ -20,11 +20,11 @@ class WeightsGradientsStatsTracker:
 
         self.reset()
 
-    def batch_statistic_upgrade(self, named_parameters: List[Tuple[str, torch.nn.parameter.Parameter]]) -> None:
+    def batch_statistic_upgrade(self, named_parameters: Iterable[Tuple[str, torch.nn.parameter.Parameter]]) -> None:
         """
         Accumulate the running mean, running variance, min and the max for all the layers.
         Args:
-             named_parameters (List[Tuple[str, ~torch.nn.parameter.Parameter]): The named parameters of the model to
+             named_parameters (Iterable[Tuple[str, ~torch.nn.parameter.Parameter]): The named parameters of the model to
              track.
         """
         batch_layer_means = []
