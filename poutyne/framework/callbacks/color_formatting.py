9da14e1644ab10aac361ee7e44f82e88c7e3d8d9
davebulaval <david.beauchemin.5@ulaval.ca>
2020-07-20 11:45:12 -0400

    remove a style resetting since will break formating in jupyter notebook

--- poutyne/framework/callbacks/color_formatting.py
+++ poutyne/framework/callbacks/color_formatting.py
@@ -144,8 +144,8 @@ class ColorProgress:
     def set_progress_bar(self, number_steps_per_epoch):
         self.steps_progress_bar = ProgressBar(
             number_steps_per_epoch,
-            bar_format="%s{percentage} |%s{bar}%s| %s" %
-            (self.text_color, self.progress_bar_color, self.text_color, Style.RESET_ALL))
+            bar_format="%s{percentage} |%s{bar}%s|" %
+                       (self.text_color, self.progress_bar_color, self.text_color))
         self.progress_bar = True
 
     def _set_epoch_formatted_text(self, epoch_number: int, epochs: int) -> None:
