e60a13a880a99da6f98d5e5a14405d768fe36f2b
Frédérik Paradis <fredy_14@live.fr>
2020-12-01 12:02:00 -0500

    Add partial blocks in progress bar

--- poutyne/framework/callbacks/progress_bar.py
+++ poutyne/framework/callbacks/progress_bar.py
@@ -1,5 +1,7 @@
 import math
-from typing import Union
+from typing import Optional
+
+BAR_EIGHTHS_CHARACTERS = [" ", "\u258f", "\u258e", "\u258d", "\u258c", "\u258b", "\u258a", "\u2589"]
 
 
 class ProgressBar:
@@ -7,10 +9,14 @@ class ProgressBar:
     ProgressBar class keeps an update of the iteration and output the corresponding update progress bar.
 
     Args:
-          steps (int): The number of steps.
-          bar_format (str, optional): User defined format for the bar_format. By default the setting is
+        steps (int): The number of steps.
+        bar_format (str, optional): User defined format for the bar_format. By default the setting is
             {percentage} |{bar}|. The two argument must be {percentage} and {bar}.
-          bar_character (str): The bar character to use. (Default value = \u2588 which is 'block'.)
+        bar_character (str): The bar character to use. (Default value = \u2588 which is 'block'.)
+        partial_bar_characters (str): A list of character to use when a fraction of the bar character
+            could be used. (Default value =
+            ``[" ", "\u258f", "\u258e", "\u258d", "\u258c", "\u258b", "\u258a", "\u2589"]``
+            that is from " " through 1/8th to 7/8th blocks)
 
     Attributes:
         total_steps (int): The total steps to do.
@@ -22,9 +28,11 @@ class ProgressBar:
 
     def __init__(self,
                  steps: int,
+                 *,
                  bar_length: int = 20,
-                 bar_format: Union[str, None] = None,
-                 bar_character: str = "\u2588") -> None:
+                 bar_format: Optional[str] = None,
+                 bar_character: str = "\u2588",
+                 partial_bar_characters: Optional[list] = None) -> None:
         self.total_steps = steps
 
         if bar_format is not None:
@@ -33,6 +41,9 @@ class ProgressBar:
             self.bar_format = "{percentage} |{bar}"
 
         self.bar_character = bar_character
+        self.partial_bar_characters = BAR_EIGHTHS_CHARACTERS
+        if partial_bar_characters is not None:
+            self.partial_bar_characters = partial_bar_characters
         self.bar_length = bar_length
 
         self.actual_steps = 0
@@ -65,6 +76,13 @@ class ProgressBar:
     def progress_bar_formatting(self) -> str:
         percentage = self.actual_steps / self.total_steps
         bar_length_complete = int(math.floor(percentage * self.bar_length)) * self.bar_character
-        bar_length_incomplete = (self.bar_length - len(bar_length_complete)) * " "
-        progress_bar = bar_length_complete + bar_length_incomplete
+        bar_length_incomplete = (self.bar_length - len(bar_length_complete) - 1) * " "
+
+        partial_character = ""
+        if percentage < 1.:
+            partial_percentage = percentage * self.bar_length - len(bar_length_complete)
+            partial_index = int(math.floor(partial_percentage * len(self.partial_bar_characters)))
+            partial_character = self.partial_bar_characters[partial_index]
+
+        progress_bar = bar_length_complete + partial_character + bar_length_incomplete
         return progress_bar
--- tests/framework/callbacks/test_progress_bar.py
+++ tests/framework/callbacks/test_progress_bar.py
@@ -8,14 +8,20 @@ class ProgressBarTest(TestCase):
 
     def test_default_bar_character_is_properly_step(self):
         smaller_bar_length = ProgressBarTest.BAR_LENGTH - 1
-        progress_bar = ProgressBar(steps=smaller_bar_length, bar_length=ProgressBarTest.BAR_LENGTH, bar_character="#")
+        progress_bar = ProgressBar(steps=smaller_bar_length,
+                                   bar_length=ProgressBarTest.BAR_LENGTH,
+                                   bar_character="#",
+                                   partial_bar_characters=[" "])
 
         progress_bar.update()
         self.assertEqual(progress_bar.progress_bar_formatting().strip(), "#")
 
     def test_bar_formatting_steps_equal_to_bar_length(self):
         equal_bar_length = ProgressBarTest.BAR_LENGTH
-        progress_bar = ProgressBar(steps=equal_bar_length, bar_length=ProgressBarTest.BAR_LENGTH, bar_character="#")
+        progress_bar = ProgressBar(steps=equal_bar_length,
+                                   bar_length=ProgressBarTest.BAR_LENGTH,
+                                   bar_character="#",
+                                   partial_bar_characters=[" "])
 
         self.assertEqual(progress_bar.progress_bar_formatting(), " " * equal_bar_length)
         for step in range(1, equal_bar_length + 1):
@@ -25,7 +31,10 @@ class ProgressBarTest(TestCase):
     def test_bar_formatting_steps_greater_than_bar_length(self):
         block_size = 2
         greater_bar_length = ProgressBarTest.BAR_LENGTH * 2
-        progress_bar = ProgressBar(steps=greater_bar_length, bar_length=ProgressBarTest.BAR_LENGTH, bar_character="#")
+        progress_bar = ProgressBar(steps=greater_bar_length,
+                                   bar_length=ProgressBarTest.BAR_LENGTH,
+                                   bar_character="#",
+                                   partial_bar_characters=[" "])
 
         self.assertEqual(progress_bar.progress_bar_formatting(), " " * ProgressBarTest.BAR_LENGTH)
         for step in range(block_size, greater_bar_length + 1, block_size):
@@ -38,7 +47,7 @@ class ProgressBarTest(TestCase):
     def test_bar_formatting_steps_greaterodd_than_bar_length(self):
         steps = 111
         bar_length = 25
-        progress_bar = ProgressBar(steps=steps, bar_length=bar_length, bar_character="#")
+        progress_bar = ProgressBar(steps=steps, bar_length=bar_length, bar_character="#", partial_bar_characters=[" "])
 
         self.assertEqual(progress_bar.progress_bar_formatting(), " " * 25)
 
@@ -61,7 +70,7 @@ class ProgressBarTest(TestCase):
     def test_other_bar_length(self):
         steps = 109
         bar_length = 20
-        progress_bar = ProgressBar(steps=steps, bar_length=bar_length, bar_character="#")
+        progress_bar = ProgressBar(steps=steps, bar_length=bar_length, bar_character="#", partial_bar_characters=[" "])
 
         self.assertEqual(progress_bar.progress_bar_formatting(), " " * 20)
 
@@ -84,7 +93,7 @@ class ProgressBarTest(TestCase):
     def test_odd_bar_length(self):
         steps = 103
         bar_length = 19
-        progress_bar = ProgressBar(steps=steps, bar_length=bar_length, bar_character="#")
+        progress_bar = ProgressBar(steps=steps, bar_length=bar_length, bar_character="#", partial_bar_characters=[" "])
 
         self.assertEqual(progress_bar.progress_bar_formatting(), " " * 19)
 
@@ -106,12 +115,37 @@ class ProgressBarTest(TestCase):
 
     def test_last_step_is_correctly_formatted(self):
         for steps in range(1, 200):
-            progress_bar = ProgressBar(steps=steps, bar_length=ProgressBarTest.BAR_LENGTH, bar_character="#")
+            progress_bar = ProgressBar(steps=steps,
+                                       bar_length=ProgressBarTest.BAR_LENGTH,
+                                       bar_character="#",
+                                       partial_bar_characters=[" "])
             progress_bar.update(steps)
             self.assertEqual(progress_bar.progress_bar_formatting(), "#" * ProgressBarTest.BAR_LENGTH + " " * 0)
 
     def test_one_before_last_step_is_only_24_bar(self):
         for steps in range(ProgressBarTest.BAR_LENGTH + 1, 200):
-            progress_bar = ProgressBar(steps=steps, bar_length=ProgressBarTest.BAR_LENGTH, bar_character="#")
+            progress_bar = ProgressBar(steps=steps,
+                                       bar_length=ProgressBarTest.BAR_LENGTH,
+                                       bar_character="#",
+                                       partial_bar_characters=[" "])
             progress_bar.update(steps - 1)
             self.assertEqual(progress_bar.progress_bar_formatting(), "#" * (ProgressBarTest.BAR_LENGTH - 1) + " " * 1)
+
+    def test_partial_bar_characters(self):
+        steps = 80
+        bar_length = 10
+        num_partials = 8
+        bar_characters = list(map(str, range(num_partials)))
+        progress_bar = ProgressBar(steps=steps,
+                                   bar_length=bar_length,
+                                   bar_character="#",
+                                   partial_bar_characters=bar_characters)
+        for step in range(steps):
+            partial_bar_character = bar_characters[step % num_partials]
+            num_characters = step // num_partials
+            self.assertEqual(len(progress_bar.progress_bar_formatting()), bar_length)
+            self.assertEqual(progress_bar.progress_bar_formatting(),
+                             "#" * num_characters + partial_bar_character + " " * (bar_length - num_characters - 1))
+            progress_bar.update()
+
+        self.assertEqual(progress_bar.progress_bar_formatting(), "#" * bar_length)
