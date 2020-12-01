2b51b23b1ee3a11b1e99659227575e2e36ab737e
Frédérik Paradis <fredy_14@live.fr>
2020-12-01 12:14:46 -0500

    Use temp directory in test_experiment

--- tests/framework/experiment/test_experiment.py
+++ tests/framework/experiment/test_experiment.py
@@ -1,14 +1,12 @@
 import os
 from unittest import TestCase
 
+from tempfile import TemporaryDirectory
+
 import torch
 from torch import nn
 
 from poutyne import Experiment
-
-test_checkpoints_path = "./tests_checkpoints"
-ckpt_1_path = os.path.join(test_checkpoints_path, "checkpoint_epoch_1.ckpt")
-ckpt_last_path = os.path.join(test_checkpoints_path, "checkpoint.ckpt")
 
 
 def some_data_tensor_generator(batch_size):
@@ -21,39 +19,51 @@ def some_data_tensor_generator(batch_size):
 class ExperimentTest(TestCase):
 
     def setUp(self):
-        self.pytorch_network = nn.Linear(1, 1)
-        self.optimizer = torch.optim.SGD(self.pytorch_network.parameters(), lr=1e-3)
-        self.loss_function = nn.MSELoss()
+        self.temp_dir_obj = TemporaryDirectory()
+        self.test_checkpoints_path = self.temp_dir_obj.name
+        self.ckpt_1_path = os.path.join(self.test_checkpoints_path, "checkpoint_epoch_1.ckpt")
+        self.ckpt_last_path = os.path.join(self.test_checkpoints_path, "checkpoint.ckpt")
 
-        self.test_experiment = Experiment(test_checkpoints_path,
-                                          self.pytorch_network,
-                                          optimizer=self.optimizer,
-                                          loss_function=self.loss_function,
-                                          monitor_metric="loss",
-                                          monitor_mode="max")
-
+        expt = Experiment(self.test_checkpoints_path,
+                          nn.Linear(1, 1),
+                          optimizer='sgd',
+                          loss_function='mse',
+                          monitor_metric="loss",
+                          monitor_mode="min")
         train_generator = some_data_tensor_generator(2)
-        self.test_experiment.train(train_generator, epochs=1)
+        expt.train(train_generator, epochs=1)
+
+        self.test_experiment = Experiment(self.test_checkpoints_path,
+                                          nn.Linear(1, 1),
+                                          optimizer='sgd',
+                                          loss_function='mse',
+                                          monitor_metric="loss",
+                                          monitor_mode="min")
+
+    def tearDown(self):
+        self.temp_dir_obj.cleanup()
 
     def test_load_checkpoint_with_int(self):
         self.test_experiment.load_checkpoint(1)
 
-        self.assertEqual(self.test_experiment.model.network.state_dict(), torch.load(ckpt_1_path, map_location="cpu"))
+        self.assertEqual(self.test_experiment.model.network.state_dict(),
+                         torch.load(self.ckpt_1_path, map_location="cpu"))
 
     def test_load_checkpoint_best(self):
         self.test_experiment.load_checkpoint("best")
 
-        self.assertEqual(self.test_experiment.model.network.state_dict(), torch.load(ckpt_1_path, map_location="cpu"))
+        self.assertEqual(self.test_experiment.model.network.state_dict(),
+                         torch.load(self.ckpt_1_path, map_location="cpu"))
 
     def test_load_checkpoint_last(self):
         self.test_experiment.load_checkpoint("last")
 
-        self.assertEqual(self.test_experiment.model.network.state_dict(), torch.load(ckpt_last_path,
-                                                                                     map_location="cpu"))
+        self.assertEqual(self.test_experiment.model.network.state_dict(),
+                         torch.load(self.ckpt_last_path, map_location="cpu"))
 
     def test_load_checkpoint_path_state_dict(self):
-        cpkt_path = os.path.join(test_checkpoints_path, "test_model_weights_state_dict.p")
-        torch.save(torch.load(ckpt_1_path, map_location="cpu"), cpkt_path)  # change the ckpt path
+        cpkt_path = os.path.join(self.test_checkpoints_path, "test_model_weights_state_dict.p")
+        torch.save(torch.load(self.ckpt_1_path, map_location="cpu"), cpkt_path)  # change the ckpt path
         self.test_experiment.load_checkpoint(cpkt_path)
 
         self.assertEqual(self.test_experiment.model.network.state_dict(), torch.load(cpkt_path, map_location="cpu"))
