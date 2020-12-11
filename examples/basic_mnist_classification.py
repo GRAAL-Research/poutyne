3a5e71b5269712beaec3386b52acc35f9c9da52b
Devansh-Walia <57324289+Devansh-Walia@users.noreply.github.com>
2020-12-11 12:40:58 +0530

    Update basic_mnist_classification.py

--- examples/basic_mnist_classification.py
+++ examples/basic_mnist_classification.py
@@ -27,6 +27,7 @@ network = nn.Sequential(
 model = Model(network, 'sgd', 'cross_entropy',
               batch_metrics=['accuracy'], epoch_metrics=['f1'])
 model.to(device)
+#change the number of epochs to find the optimum value for your work
 model.fit_dataset(
     train_dataset,
     valid_dataset,
