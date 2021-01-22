4e82fe42bb49375858ddfbff5a6ff900a07f88a3
davebulaval <david.beauchemin.5@ulaval.ca>
2021-01-22 18:02:26 -0500

    fixed epoch in timing

--- poutyne/framework/model.py
+++ poutyne/framework/model.py
@@ -578,9 +578,8 @@ class Model:
                 callback_list.on_valid_begin({})
                 self._validate(valid_step_iterator)
 
-                valid_total_time = timeit.default_timer() - valid_begin_time
-
                 valid_step_iterator.epoch_metrics = self._get_epoch_metrics()
+                valid_total_time = timeit.default_timer() - valid_begin_time
 
                 valid_metrics_log = {'time': valid_total_time}
                 valid_metrics_log.update(valid_step_iterator.metrics_logs)
@@ -635,8 +634,10 @@ class Model:
                 callback_list.on_valid_begin({})
                 valid_begin_time = timeit.default_timer()
                 self._validate(valid_step_iterator)
-                valid_total_time = timeit.default_timer() - valid_begin_time
+
                 valid_step_iterator.epoch_metrics = self._get_epoch_metrics()
+                valid_total_time = timeit.default_timer() - valid_begin_time
+                
                 valid_metrics_log = {'time': valid_total_time}
                 valid_metrics_log.update(valid_step_iterator.metrics_logs)
 
@@ -1147,9 +1148,9 @@ class Model:
         loss, batch_metrics, pred_y, true_y = self._validate(step_iterator,
                                                              return_pred=return_pred,
                                                              return_ground_truth=return_ground_truth)
-        test_total_time = timeit.default_timer() - test_begin_time
 
         step_iterator.epoch_metrics = self._get_epoch_metrics()
+        test_total_time = timeit.default_timer() - test_begin_time
 
         if return_pred and concatenate_returns:
             pred_y = _concat(pred_y)
