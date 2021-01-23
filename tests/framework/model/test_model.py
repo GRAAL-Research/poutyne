58b5a1dc74b92d66e73ff3b1732b68d61523e70e
Frédérik Paradis <fredy_14@live.fr>
2021-01-23 13:54:57 -0500

    Test evaluate_generator with StopIteration exception

--- tests/framework/model/test_model.py
+++ tests/framework/model/test_model.py
@@ -887,6 +887,13 @@ class ModelTest(ModelFittingTestCase):
         self.assertEqual(type(pred_y), np.ndarray)
         self.assertEqual(pred_y.shape, (num_steps * ModelTest.batch_size, 1))
 
+    def test_evaluate_generator_with_stop_iteration(self):
+        test_generator = SomeDataGeneratorUsingStopIteration(ModelTest.batch_size, 10)
+
+        loss, _ = self.model.evaluate_generator(test_generator)
+
+        self.assertEqual(type(loss), float)
+
     def test_evaluate_generator_with_progress_bar_coloring(self):
         num_steps = 10
         generator = some_data_tensor_generator(ModelTest.batch_size)
@@ -1209,13 +1216,6 @@ class ModelTest(ModelFittingTestCase):
             inf_batch_size = self.model.get_batch_size([1, 2, 3], [4, 5, 6])
             self.assertEqual(inf_batch_size, 1)
             self.assertEqual(len(w), 0)
-
-    def test_evaluate_generator_with_iterator(self):
-        test_generator = some_data_tensor_generator(ModelTest.batch_size)
-
-        loss, _ = self.model.evaluate_generator(test_generator, steps=10)
-
-        self.assertEqual(type(loss), float)
 
 
 @skipIf(MNIST is None, "Unable to import MNIST")
