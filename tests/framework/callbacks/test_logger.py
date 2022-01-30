93eda86e60b7cff4182a692b31850a7ce2f5a19f
Frédérik Paradis <fredy_14@live.fr>
2022-01-30 13:31:16 -0500

    Add train_val_split to TensorBoardLogger

--- tests/framework/callbacks/test_logger.py
+++ tests/framework/callbacks/test_logger.py
@@ -211,5 +211,76 @@ class TorchTensorboardLoggerTest(BaseTensorBoardLoggerTest, TestCase):
     SummaryWriter = TorchSummaryWriter
 
 
+class BaseTensorBoardLoggerWithSplitTrainValTest:
+    SummaryWriter = None
+    batch_size = 20
+    lr = 1e-3
+    num_epochs = 10
+
+    def setUp(self):
+        torch.manual_seed(42)
+        self.pytorch_network = nn.Linear(1, 1)
+        self.loss_function = nn.MSELoss()
+        self.optimizer = torch.optim.SGD(self.pytorch_network.parameters(), lr=BaseTensorBoardLoggerTest.lr)
+        self.model = Model(self.pytorch_network, self.optimizer, self.loss_function)
+        self.temp_dir_obj = TemporaryDirectory()
+        # pylint: disable=not-callable
+        self.writer = self.SummaryWriter(self.temp_dir_obj.name)
+        self.writer.add_scalar = MagicMock()
+
+    def tearDown(self):
+        self.temp_dir_obj.cleanup()
+
+    def test_logging(self):
+        train_gen = some_data_generator(20)
+        valid_gen = some_data_generator(20)
+        logger = TensorBoardLogger(self.writer, split_train_val=True)
+        history = self.model.fit_generator(
+            train_gen, valid_gen, epochs=self.num_epochs, steps_per_epoch=5, callbacks=[logger]
+        )
+        self._test_logging(history)
+
+    def test_multiple_learning_rates(self):
+        train_gen = some_data_generator(20)
+        valid_gen = some_data_generator(20)
+        logger = TensorBoardLogger(self.writer, split_train_val=True)
+        lrs = [BaseCSVLoggerTest.lr, BaseCSVLoggerTest.lr / 2]
+        optimizer = torch.optim.SGD(
+            [dict(params=[self.pytorch_network.weight], lr=lrs[0]), dict(params=[self.pytorch_network.bias], lr=lrs[1])]
+        )
+        model = Model(self.pytorch_network, optimizer, self.loss_function)
+        history = model.fit_generator(
+            train_gen, valid_gen, epochs=self.num_epochs, steps_per_epoch=5, callbacks=[logger]
+        )
+        self._test_logging(history, lrs=lrs)
+
+    def _test_logging(self, history, lrs=None):
+        if lrs is None:
+            lrs = [BaseCSVLoggerTest.lr]
+
+        calls = []
+        for h in history:
+            for k, v in h.items():
+                calls.append(call(k, v, h['epoch']))
+
+            if len(lrs) == 1:
+                calls.append(call('lr', self.lr, h['epoch']))
+            else:
+                for i, lr in enumerate(lrs):
+                    calls.append(call(f'lr_group_{i}', lr, h['epoch']))
+
+        self.writer.add_scalar.assert_has_calls(calls, any_order=True)
+
+
+@skipIf(XSummaryWriter is None, "Needs tensorboardX to run this test")
+class TensorboardXLoggerWithSplitTrainValTest(BaseTensorBoardLoggerWithSplitTrainValTest, TestCase):
+    SummaryWriter = XSummaryWriter
+
+
+@skipIf(TorchSummaryWriter is None, "Unable to import SummaryWriter from torch")
+class TorchTensorboardLoggerWithSplitTrainValTest(BaseTensorBoardLoggerWithSplitTrainValTest, TestCase):
+    SummaryWriter = TorchSummaryWriter
+
+
 if __name__ == '__main__':
     main()
