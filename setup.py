ebd1d4b34efc7234e36da1bb3eb3193adf314f8f
Frédérik Paradis <fredy_14@live.fr>
2022-04-28 16:13:08 -0400

    Add Python 3.10 in setuptools classifiers

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ def main():
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
             'Topic :: Software Development :: Libraries',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Scientific/Engineering',
